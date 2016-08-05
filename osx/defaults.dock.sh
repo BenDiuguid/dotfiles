1ab0701167a10850b5903d42f2f68136303415c4
Ben Diuguid <bendiuguid@gmail.com>
2016-08-04 21:47:39 -0400

    tweak dock settings

--- osx/defaults.dock.sh
+++ osx/defaults.dock.sh
@@ -3,17 +3,13 @@
 dockutil --no-restart --remove all
 dockutil --no-restart --add "/Applications/System Preferences.app"
 
-dockutil --no-restart --add "/Applications/Safari Technology Preview.app"
 dockutil --no-restart --add "$HOME/Applications/Brave.app"
-dockutil --no-restart --add "$HOME/Applications/Vivaldi.app"
 dockutil --no-restart --add "$HOME/Applications/Google Chrome.app"
 
 dockutil --no-restart --add "$HOME/Applications/Atom.app"
-dockutil --no-restart --add "$HOME/Applications/WebStorm.app"
-dockutil --no-restart --add "$HOME/Applications/Visual Studio Code.app"
-dockutil --no-restart --add "/Applications/Utilities/Terminal.app"
+dockutil --no-restart --add "$HOME/Applications/HyperTerm.app"
+# dockutil --no-restart --add "/Applications/Utilities/Terminal.app"
 
-dockutil --no-restart --add "$HOME/Applications/GitHub Desktop.app"
 dockutil --no-restart --add "$HOME/Applications/Slack.app"
 dockutil --no-restart --add "/Applications/App Store.app"
 
